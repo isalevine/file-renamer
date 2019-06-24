5b597c7c0b2002ce0940a7d50d52d4aa3c38be29
isalevine <isa.loves.coding@gmail.com>
2019-06-23 23:28:17 -0700

    added notes about how to structure menu/CLI commands

--- script.py
+++ script.py
@@ -30,3 +30,16 @@ print("Directory name chosen: " + input)
 
 if (currentDirectory / input).exists():
     print("true!!")
+
+
+# IDEA for CLI TO NAVIGATE DIRECTORY:
+# -> recursively call an update_directory() function
+#    that will append/mutate based on user input, and
+#    automatically call the print_menu() function again
+# -> once a given directory is selected/"locked in",
+#    have user input a string to match (i.e. "Snare")
+#    and confirm a list of matching files
+# -> then, specify the mutation with a CLI command:
+#    $ replace "Snare " with ""
+#    and take the first "" and second "" as parameters
+#    for calling change_filenames(list, string1, string2)
