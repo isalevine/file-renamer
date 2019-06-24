530cc3c6275830bff469024a51334c57cd8ba7f3
isalevine <isa.loves.coding@gmail.com>
2019-06-23 23:21:05 -0700

    added user input, can check if directory name exists

--- script.py
+++ script.py
@@ -6,10 +6,27 @@
 #     print(text.read())
 
 
+
+
 # per: https://stackabuse.com/python-list-files-in-a-directory/
 import pathlib
 
-currentDirectory = pathlib.Path('.')
+# GOAL: create CLI to navigate & select directory,
+# set Regex/string-matching for filenames, and
+# replace (look for GSUB equivalent?) with specified text
+path = './drum-samples'
+
+currentDirectory = pathlib.Path(path)
+
+currentDirectory = currentDirectory / "SDS800 Kits"
 
 for filename in currentDirectory.iterdir():
     print (filename)
+
+input = input("Enter directory name: ")
+print("Directory name chosen: " + input)
+
+# print(type(currentDirectory))
+
+if (currentDirectory / input).exists():
+    print("true!!")
