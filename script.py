f4d4b1c177110323c3e244500df34e6d723e0e7c
isalevine <isa.loves.coding@gmail.com>
2019-06-23 22:50:12 -0700

    printing all filenames in directory

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
-with open('test-file.txt', 'a') as text:
-    text.write(", isa")
+# with open('test-file.txt', 'a') as text:
+#     text.write(", isa")
 
 
-with open('test-file.txt', 'r') as text:
-    print(text.read())
+# with open('test-file.txt', 'r') as text:
+#     print(text.read())
+
+
+# per: https://stackabuse.com/python-list-files-in-a-directory/
+import pathlib
+
+currentDirectory = pathlib.Path('.')
+
+for filename in currentDirectory.iterdir():
+    print (filename)
